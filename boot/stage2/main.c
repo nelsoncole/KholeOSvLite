b23202c278e3a2f7f7cb400715d6ee0305255418
nelsoncole <nelsoncole72@gmail.com>
2017-07-20 17:20:18 +0100

    Create main.c

---
+++ boot/stage2/main.c
@@ -0,0 +1,25 @@
+/* Arquivo "main.c"
+	"Projecto KholeOS. Bootloader!"
+	
+	Nelson Sapalo da Silva Cole (nelsoncole72@gmail.com)
+	
+	Lubango, 20 de Julho de 2017
+*/
+
+#include <boot.h>
+
+
+
+int main(){
+
+	set_color(0x2);
+	puts("Projecto KholeOS. Bootloader!");
+
+
+/* Vamos programar um printf (nosso caso será printboot)
+
+*/
+
+	return 0;	
+
+	}
