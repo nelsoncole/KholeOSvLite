130ebbd21fd06e66e283ef28ebd8f2000eb17228
nelsoncole <nelsoncole72@gmail.com>
2017-07-20 17:20:58 +0100

    Create io.c

---
+++ boot/stage2/io.c
@@ -0,0 +1,27 @@
+
+
+/* Arquivo "io.c"
+   
+	"Projecto KholeOS, Bootloader!"
+	
+	Nelson Sapalo da Silva Cole (nelsoncole72@gmail.com  +244-948-833-132)
+
+  	Lubango, 18 de Julho de 2017
+
+*/
+
+
+#include <boot.h>
+
+
+/*
+
+
+
+
+	Aqui iremos programar o driver de disco usando o modo PIO
+
+
+
+
+*/
